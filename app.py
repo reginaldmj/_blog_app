81b5e36c56e50876f8c51f5f694ef4d35a5c6758
Reginald <reginald.jeffersonm@gmail.com>
2026-05-25 22:28:12 -0500

    Implement user registration

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
-from models import db
+from flask import Flask, render_template, redirect, url_for
+from werkzeug.security import generate_password_hash
+from models import db, User
+from forms import RegisterForm
 
 app = Flask(__name__)
 
@@ -16,6 +18,26 @@ with app.app_context():
 @app.route('/')
 def home():
     return render_template('index.html')
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data)
+
+        user = User(
+            username=form.username.data,
+            email=form.email.data,
+            password=hashed_password
+        )
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    return render_template('register.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
