779aa4bbbf8c6f062fc979c49e4ce5ea0c4cc9ce
Reginald <reginald.jeffersonm@gmail.com>
2026-05-26 19:09:09 -0500

    implemented login and logout system

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, redirect, url_for
-from werkzeug.security import generate_password_hash
-from models import db, User
-from forms import RegisterForm
+from flask import Flask, render_template, redirect, url_for, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from models import db, User, Post
+from forms import RegisterForm, LoginForm
 
 app = Flask(__name__)
 
@@ -14,6 +15,16 @@ db.init_app(app)
 
 with app.app_context():
     db.create_all()
+
+# Initialize Login Manager
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 @app.route('/')
 def home():
@@ -38,6 +49,33 @@ def register():
         return redirect(url_for('home'))
 
     return render_template('register.html', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(email=form.email.data).first()
+
+        if user and check_password_hash(user.password, form.password.data):
+            login_user(user)
+
+            flash('Login successful')
+
+            return redirect(url_for('home'))
+
+        flash('Invalid credentials')
+
+    return render_template('login.html', form=form
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+
+    flash('Logged out successfully')
+
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
--- forms.py
+++ forms.py
@@ -7,3 +7,8 @@ class RegisterForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Register')
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
